912882a2ae663290505b57eea858bce10d1432b9
zoint <freelancerzoint@gmail.com>
2018-10-09 12:34:24 +0200

    Basic Login Page added, Registration is removed because of security issues

---
+++ FraunhoferLogin/src/app/app.module.ts
@@ -0,0 +1,40 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule }    from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+//import { fakeBackendProvider } from './_helpers';
+
+import { AppComponent }  from './app.component';
+import { routing }        from './app.routing';
+
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { OpcuainfoComponent} from "./opcuainfo/opcuainfo.component";
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        //You can put a fake backend here
+        //fake-backend
+
+    ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
